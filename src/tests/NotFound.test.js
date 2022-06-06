bf3bf781f96693c712ae65def8cbed8b6b06cba1
Erika de Oliveira Lima <erikadeo.lima@hotmail.com>
2022-06-06 14:47:39 -0300

    requisito NotFound: testes ok, stryker 100%

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found 😭', () => {
+    render(<NotFound />);
+
+    const notFound = screen.getByRole('heading', {name:/page requested not found/i, level:2});
+    
+    expect(notFound).toBeInTheDocument();
+  });
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    render(<NotFound />);
+
+    const img = screen.getAllByRole('img')[1];
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(img).toHaveAttribute('src', url);
+  });
+});
+
